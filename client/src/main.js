6c85cfb107ede609218da59a35e633b425d53a5b
Arcoders <anonismaa@gmail.com>
2018-07-26 11:29:41 +0200

    i & configure vue client

---
+++ client/src/main.js
@@ -0,0 +1,20 @@
+
+import { sync } from 'vuex-router-sync';
+import Vuetify from 'vuetify';
+
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store/index';
+
+Vue.use(Vuetify);
+
+sync(store, router);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app');
