0710218429e0a6f1d218fb5c1a7653baa80836f3
Arcoders <anonismaa@gmail.com>
2018-07-26 13:15:31 +0200

    component - register layout

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -5,13 +5,13 @@
       <v-toolbar-title class="mr-4">VUE TODO</v-toolbar-title>
       <v-toolbar-items>
         <v-btn flat>
-          <v-icon>playlist_add_check</v-icon>
+          <v-icon class="mr-2">playlist_add_check</v-icon>
           Projects
         </v-btn>
       </v-toolbar-items>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>
+        <v-btn flat to="/register">
           <v-icon class="mr-2">account_box</v-icon>
           Register
         </v-btn>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div>
+    <v-container>
+        <v-layout row wrap>
+            <v-flex xs6 offset-xs3>
+                <h1>Register</h1>
+                <v-text-field lable="email" placeholder="Email"></v-text-field>
+                <v-text-field lable="Password" placeholder="Password" type="password"></v-text-field>
+                <v-btn color="green" dark>
+                    <v-icon class="mr-2">account_circle</v-icon>
+                    Register
+                 </v-btn>
+            </v-flex>
+        </v-layout>
+    </v-container>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+    
+}
+
+</script>
+
